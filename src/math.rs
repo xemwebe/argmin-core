61ca36da9c9e9176fa57abef49787cbd841a283d
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-08-25 10:13:28 +0200

    let macros do the work

--- src/math.rs
+++ src/math.rs
@@ -13,74 +13,84 @@ pub trait ArgminDot<T, U> {
     fn dot(&self, T) -> U;
 }
 
-impl ArgminDot<Vec<f64>, f64> for Vec<f64> {
-    fn dot(&self, other: Vec<f64>) -> f64 {
-        self.iter().zip(other.iter()).map(|(a, b)| a * b).sum()
-    }
-}
-
-impl<'a> ArgminDot<&'a Vec<f64>, f64> for Vec<f64> {
-    fn dot(&self, other: &'a Vec<f64>) -> f64 {
-        self.iter().zip(other.iter()).map(|(a, b)| a * b).sum()
-    }
+pub trait ArgminAdd<T> {
+    fn sub(&self, T) -> Self;
 }
 
 pub trait ArgminSub<T> {
     fn sub(&self, T) -> Self;
 }
 
-impl ArgminSub<Vec<f64>> for Vec<f64> {
-    fn sub(&self, other: Vec<f64>) -> Vec<f64> {
-        self.iter().zip(other.iter()).map(|(a, b)| a - b).collect()
-    }
-}
-
-impl<'a> ArgminSub<&'a Vec<f64>> for Vec<f64> {
-    fn sub(&self, other: &'a Vec<f64>) -> Vec<f64> {
-        self.iter().zip(other.iter()).map(|(a, b)| a - b).collect()
-    }
-}
-
 pub trait ArgminScaledAdd<T, U> {
     fn scaled_add(&self, U, T) -> Self;
-}
-
-impl ArgminScaledAdd<Vec<f64>, f64> for Vec<f64> {
-    fn scaled_add(&self, scale: f64, other: Vec<f64>) -> Vec<f64> {
-        self.iter()
-            .zip(other.iter())
-            .map(|(a, b)| a + scale * b)
-            .collect()
-    }
-}
-
-impl<'a> ArgminScaledAdd<&'a Vec<f64>, f64> for Vec<f64> {
-    fn scaled_add(&self, scale: f64, other: &'a Vec<f64>) -> Vec<f64> {
-        self.iter()
-            .zip(other.iter())
-            .map(|(a, b)| a + scale * b)
-            .collect()
-    }
 }
 
 pub trait ArgminScaledSub<T, U> {
     fn scaled_sub(&self, U, T) -> Self;
 }
 
-impl ArgminScaledSub<Vec<f64>, f64> for Vec<f64> {
-    fn scaled_sub(&self, scale: f64, other: Vec<f64>) -> Vec<f64> {
-        self.iter()
-            .zip(other.iter())
-            .map(|(a, b)| a - scale * b)
-            .collect()
-    }
+macro_rules! make_math {
+    ($t:ty, $u:ty, $v:ty) => {
+        impl<'a> ArgminDot<$t, $u> for $v {
+            fn dot(&self, other: $t) -> $u {
+                self.iter().zip(other.iter()).map(|(a, b)| a * b).sum()
+            }
+        }
+
+        impl<'a> ArgminAdd<$t> for $v {
+            fn sub(&self, other: $t) -> $v {
+                self.iter().zip(other.iter()).map(|(a, b)| a + b).collect()
+            }
+        }
+
+        impl<'a> ArgminSub<$t> for $v {
+            fn sub(&self, other: $t) -> $v {
+                self.iter().zip(other.iter()).map(|(a, b)| a - b).collect()
+            }
+        }
+
+        impl<'a> ArgminScaledAdd<$t, $u> for $v {
+            fn scaled_add(&self, scale: $u, other: $t) -> $v {
+                self.iter()
+                    .zip(other.iter())
+                    .map(|(a, b)| a + scale * b)
+                    .collect()
+            }
+        }
+
+        impl<'a> ArgminScaledSub<$t, $u> for $v {
+            fn scaled_sub(&self, scale: $u, other: $t) -> $v {
+                self.iter()
+                    .zip(other.iter())
+                    .map(|(a, b)| a - scale * b)
+                    .collect()
+            }
+        }
+    };
 }
 
-impl<'a> ArgminScaledSub<&'a Vec<f64>, f64> for Vec<f64> {
-    fn scaled_sub(&self, scale: f64, other: &Vec<f64>) -> Vec<f64> {
-        self.iter()
-            .zip(other.iter())
-            .map(|(a, b)| a + scale * b)
-            .collect()
-    }
-}
+// Not sure if all of this makes any sense...
+make_math!(Vec<f32>, f32, Vec<f32>);
+make_math!(Vec<f64>, f64, Vec<f64>);
+make_math!(Vec<i8>, i8, Vec<i8>);
+make_math!(Vec<i16>, i16, Vec<i16>);
+make_math!(Vec<i32>, i32, Vec<i32>);
+make_math!(Vec<i64>, i64, Vec<i64>);
+make_math!(Vec<u8>, u8, Vec<u8>);
+make_math!(Vec<u16>, u16, Vec<u16>);
+make_math!(Vec<u32>, u32, Vec<u32>);
+make_math!(Vec<u64>, u64, Vec<u64>);
+make_math!(Vec<isize>, isize, Vec<isize>);
+make_math!(Vec<usize>, usize, Vec<usize>);
+make_math!(&'a Vec<f32>, f32, Vec<f32>);
+make_math!(&'a Vec<f64>, f64, Vec<f64>);
+make_math!(&'a Vec<i8>, i8, Vec<i8>);
+make_math!(&'a Vec<i16>, i16, Vec<i16>);
+make_math!(&'a Vec<i32>, i32, Vec<i32>);
+make_math!(&'a Vec<i64>, i64, Vec<i64>);
+make_math!(&'a Vec<u8>, u8, Vec<u8>);
+make_math!(&'a Vec<u16>, u16, Vec<u16>);
+make_math!(&'a Vec<u32>, u32, Vec<u32>);
+make_math!(&'a Vec<u64>, u64, Vec<u64>);
+make_math!(&'a Vec<isize>, isize, Vec<isize>);
+make_math!(&'a Vec<usize>, usize, Vec<usize>);
