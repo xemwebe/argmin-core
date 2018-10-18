96a2db1e839a6d28af9998288ed1b88596127c26
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-10-18 13:10:16 +0200

    removed radius again (was stupid)

--- src/lib.rs
+++ src/lib.rs
@@ -380,9 +380,6 @@ pub trait ArgminTrustRegion: ArgminSolver {
     /// Set the initial step length
     fn set_radius(&mut self, f64);
 
-    /// Set the initial step length
-    fn radius(&self) -> f64;
-
     /// Set the gradient at the starting point
     fn set_grad(&mut self, <Self as ArgminNextIter>::Parameters);
 
