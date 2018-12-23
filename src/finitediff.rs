893dec4e066fdd94d392b581e73cdf3ecd2c0078
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-12-23 18:37:38 +0100

    implemented forward diff for ndarray::Arra1<f64>

--- src/finitediff.rs
+++ src/finitediff.rs
@@ -11,6 +11,8 @@
 
 use crate::ArgminOperator;
 use crate::Error;
+#[cfg(feature = "ndarrayl")]
+use ndarray;
 
 pub trait ArgminForwardDiff
 where
@@ -23,6 +25,28 @@ where
 }
 
 impl ArgminForwardDiff for Vec<f64>
+where
+    Self: Sized,
+{
+    fn forward_diff<H>(
+        &self,
+        op: &ArgminOperator<Parameters = Self, OperatorOutput = f64, Hessian = H>,
+    ) -> Result<Self, Error> {
+        let fx = op.apply(&self)?;
+        let n = self.len();
+        Ok((0..n)
+            .map(|i| {
+                let mut x1 = self.clone();
+                x1[i] += std::f64::EPSILON;
+                let fx1 = op.apply(&x1).unwrap();
+                (fx1 - fx) / std::f64::EPSILON
+            })
+            .collect())
+    }
+}
+
+#[cfg(feature = "ndarrayl")]
+impl ArgminForwardDiff for ndarray::Array1<f64>
 where
     Self: Sized,
 {
@@ -67,6 +91,34 @@ mod tests {
         }
         let prob = Problem {};
         let p = vec![0.0f64, 1.0f64];
+        // println!("{:?}", prob.gradient(&p).unwrap());
+        assert!((1.0 - prob.gradient(&p).unwrap()[0]).abs() < std::f64::EPSILON);
+        assert!((2.0 - prob.gradient(&p).unwrap()[1]).abs() < std::f64::EPSILON);
+    }
+
+    #[cfg(feature = "ndarrayl")]
+    #[test]
+    fn test_forward_diff_ndarray1_f64() {
+        use ndarray;
+
+        #[derive(Clone)]
+        struct Problem {}
+
+        impl ArgminOperator for Problem {
+            type Parameters = ndarray::Array1<f64>;
+            type OperatorOutput = f64;
+            type Hessian = ();
+
+            fn apply(&self, p: &Self::Parameters) -> Result<Self::OperatorOutput, Error> {
+                Ok(p[0] + p[1].powi(2))
+            }
+
+            fn gradient(&self, p: &Self::Parameters) -> Result<Self::Parameters, Error> {
+                p.forward_diff(self)
+            }
+        }
+        let prob = Problem {};
+        let p = ndarray::Array1::from_vec(vec![0.0f64, 1.0f64]);
         // println!("{:?}", prob.gradient(&p).unwrap());
         assert!((1.0 - prob.gradient(&p).unwrap()[0]).abs() < std::f64::EPSILON);
         assert!((2.0 - prob.gradient(&p).unwrap()[1]).abs() < std::f64::EPSILON);
