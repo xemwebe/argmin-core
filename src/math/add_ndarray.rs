fff5c061199318dd7dd885c7d5adfd1660885bb5
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-01-30 15:00:18 +0100

    tests

--- src/math/add_ndarray.rs
+++ src/math/add_ndarray.rs
@@ -72,32 +72,32 @@ mod tests {
                 }
             }
 
-            // item! {
-            //     #[test]
-            //     fn [<test_add_scalar_vec_ $t>]() {
-            //         let a = vec![1 as $t, 4 as $t, 8 as $t];
-            //         let b = 34 as $t;
-            //         let target = vec![35 as $t, 38 as $t, 42 as $t];
-            //         let res = <$t as ArgminAdd<Vec<$t>, Vec<$t>>>::add(&b, &a);
-            //         for i in 0..3 {
-            //             assert!(((target[i] - res[i]) as f64).abs() < std::f64::EPSILON);
-            //         }
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     fn [<test_add_vec_vec_ $t>]() {
-            //         let a = vec![1 as $t, 4 as $t, 8 as $t];
-            //         let b = vec![41 as $t, 38 as $t, 34 as $t];
-            //         let target = vec![42 as $t, 42 as $t, 42 as $t];
-            //         let res = <Vec<$t> as ArgminAdd<Vec<$t>, Vec<$t>>>::add(&a, &b);
-            //         for i in 0..3 {
-            //             assert!(((target[i] - res[i]) as f64).abs() < std::f64::EPSILON);
-            //         }
-            //     }
-            // }
-            //
+            item! {
+                #[test]
+                fn [<test_add_scalar_vec_ $t>]() {
+                    let a = array![1 as $t, 4 as $t, 8 as $t];
+                    let b = 34 as $t;
+                    let target = array![35 as $t, 38 as $t, 42 as $t];
+                    let res = <$t as ArgminAdd<Array1<$t>, Array1<$t>>>::add(&b, &a);
+                    for i in 0..3 {
+                        assert!(((target[i] - res[i]) as f64).abs() < std::f64::EPSILON);
+                    }
+                }
+            }
+
+            item! {
+                #[test]
+                fn [<test_add_vec_vec_ $t>]() {
+                    let a = array![1 as $t, 4 as $t, 8 as $t];
+                    let b = array![41 as $t, 38 as $t, 34 as $t];
+                    let target = array![42 as $t, 42 as $t, 42 as $t];
+                    let res = <Array1<$t> as ArgminAdd<Array1<$t>, Array1<$t>>>::add(&a, &b);
+                    for i in 0..3 {
+                        assert!(((target[i] - res[i]) as f64).abs() < std::f64::EPSILON);
+                    }
+                }
+            }
+
             // item! {
             //     #[test]
             //     #[should_panic]
