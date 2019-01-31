3332c89fcbdbd7c0b88fabde057ea769c7ed274e
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-01-31 10:30:24 +0100

    tests

--- src/math/div_vec.rs
+++ src/math/div_vec.rs
@@ -146,73 +146,73 @@ mod tests {
                 }
             }
 
-            // item! {
-            //     #[test]
-            //     fn [<test_div_mat_mat_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 4 as $t, 8 as $t],
-            //             vec![2 as $t, 5 as $t, 9 as $t]
-            //         ];
-            //         let b = vec![
-            //             vec![2 as $t, 3 as $t, 4 as $t],
-            //             vec![3 as $t, 4 as $t, 5 as $t]
-            //         ];
-            //         let target = vec![
-            //             vec![2 as $t, 12 as $t, 32 as $t],
-            //             vec![6 as $t, 20 as $t, 45 as $t]
-            //         ];
-            //         let res = <Vec<Vec<$t>> as ArgminDiv<Vec<Vec<$t>>, Vec<Vec<$t>>>>::div(&a, &b);
-            //         for i in 0..3 {
-            //             for j in 0..2 {
-            //             assert!(((target[j][i] - res[j][i]) as f64).abs() < std::f64::EPSILON);
-            //             }
-            //         }
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     #[should_panic]
-            //     fn [<test_div_mat_mat_panic_1_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 4 as $t, 8 as $t],
-            //             vec![2 as $t, 9 as $t]
-            //         ];
-            //         let b = vec![
-            //             vec![41 as $t, 38 as $t, 34 as $t],
-            //             vec![40 as $t, 37 as $t, 33 as $t]
-            //         ];
-            //         <Vec<Vec<$t>> as ArgminDiv<Vec<Vec<$t>>, Vec<Vec<$t>>>>::div(&a, &b);
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     #[should_panic]
-            //     fn [<test_div_mat_mat_panic_2_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 4 as $t, 8 as $t],
-            //             vec![2 as $t, 5 as $t, 9 as $t]
-            //         ];
-            //         let b = vec![
-            //             vec![41 as $t, 38 as $t, 34 as $t],
-            //         ];
-            //         <Vec<Vec<$t>> as ArgminDiv<Vec<Vec<$t>>, Vec<Vec<$t>>>>::div(&a, &b);
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     #[should_panic]
-            //     fn [<test_div_mat_mat_panic_3_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 4 as $t, 8 as $t],
-            //             vec![2 as $t, 5 as $t, 9 as $t]
-            //         ];
-            //         let b = vec![];
-            //         <Vec<Vec<$t>> as ArgminDiv<Vec<Vec<$t>>, Vec<Vec<$t>>>>::div(&a, &b);
-            //     }
-            // }
+            item! {
+                #[test]
+                fn [<test_div_mat_mat_ $t>]() {
+                    let a = vec![
+                        vec![4 as $t, 12 as $t, 8 as $t],
+                        vec![9 as $t, 20 as $t, 45 as $t]
+                    ];
+                    let b = vec![
+                        vec![2 as $t, 3 as $t, 4 as $t],
+                        vec![3 as $t, 4 as $t, 5 as $t]
+                    ];
+                    let target = vec![
+                        vec![2 as $t, 4 as $t, 2 as $t],
+                        vec![3 as $t, 5 as $t, 9 as $t]
+                    ];
+                    let res = <Vec<Vec<$t>> as ArgminDiv<Vec<Vec<$t>>, Vec<Vec<$t>>>>::div(&a, &b);
+                    for i in 0..3 {
+                        for j in 0..2 {
+                        assert!(((target[j][i] - res[j][i]) as f64).abs() < std::f64::EPSILON);
+                        }
+                    }
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_div_mat_mat_panic_1_ $t>]() {
+                    let a = vec![
+                        vec![1 as $t, 4 as $t, 8 as $t],
+                        vec![2 as $t, 9 as $t]
+                    ];
+                    let b = vec![
+                        vec![41 as $t, 38 as $t, 34 as $t],
+                        vec![40 as $t, 37 as $t, 33 as $t]
+                    ];
+                    <Vec<Vec<$t>> as ArgminDiv<Vec<Vec<$t>>, Vec<Vec<$t>>>>::div(&a, &b);
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_div_mat_mat_panic_2_ $t>]() {
+                    let a = vec![
+                        vec![1 as $t, 4 as $t, 8 as $t],
+                        vec![2 as $t, 5 as $t, 9 as $t]
+                    ];
+                    let b = vec![
+                        vec![41 as $t, 38 as $t, 34 as $t],
+                    ];
+                    <Vec<Vec<$t>> as ArgminDiv<Vec<Vec<$t>>, Vec<Vec<$t>>>>::div(&a, &b);
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_div_mat_mat_panic_3_ $t>]() {
+                    let a = vec![
+                        vec![1 as $t, 4 as $t, 8 as $t],
+                        vec![2 as $t, 5 as $t, 9 as $t]
+                    ];
+                    let b = vec![];
+                    <Vec<Vec<$t>> as ArgminDiv<Vec<Vec<$t>>, Vec<Vec<$t>>>>::div(&a, &b);
+                }
+            }
         };
     }
 
