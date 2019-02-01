0775f100f06f42bc7d43dafee0d1263835fc66fc
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-02-01 12:04:17 +0100

    test

--- src/math/scaledadd_ndarray.rs
+++ src/math/scaledadd_ndarray.rs
@@ -8,7 +8,7 @@
 #[cfg(test)]
 mod tests {
     use crate::math::ArgminScaledAdd;
-    use ndarray::{array, Array1};
+    use ndarray::{array, Array1, Array2};
     use paste::item;
 
     macro_rules! make_test {
@@ -63,209 +63,91 @@ mod tests {
                 }
             }
 
-        //     item! {
-        //         #[test]
-        //         #[should_panic]
-        //         fn [<test_scaledadd_vec_vec_panic_1_ $t>]() {
-        //             let a = array![1 as $t, 2 as $t];
-        //             let b = array![3 as $t, 2 as $t, 1 as $t];
-        //             let c = array![4 as $t, 5 as $t, 6 as $t];
-        //             a.scaled_add(&b, &c);
-        //         }
-        //     }
-        //
-        //     item! {
-        //         #[test]
-        //         #[should_panic]
-        //         fn [<test_scaledadd_vec_vec_panic_2_ $t>]() {
-        //             let a = array![1 as $t, 2 as $t, 3 as $t];
-        //             let b = array![3 as $t, 2 as $t];
-        //             let c = array![4 as $t, 5 as $t, 6 as $t];
-        //             a.scaled_add(&b, &c);
-        //         }
-        //     }
-        //
-        //     item! {
-        //         #[test]
-        //         #[should_panic]
-        //         fn [<test_scaledadd_vec_vec_panic_3_ $t>]() {
-        //             let a = array![1 as $t, 2 as $t, 3 as $t];
-        //             let b = array![3 as $t, 2 as $t, 1 as $t];
-        //             let c = array![4 as $t, 5 as $t];
-        //             a.scaled_add(&b, &c);
-        //         }
-        //     }
-        //
-        //     item! {
-        //         #[test]
-        //         fn [<test_scaledadd_mat_mat_ $t>]() {
-        //             let a = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [3 as $t, 4 as $t],
-        //             ];
-        //             let b = array![
-        //                 [4 as $t, 3 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             let c = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             let res = a.scaled_add(&b, &c);
-        //             let target = array![
-        //                 [5 as $t, 8 as $t],
-        //                 [7 as $t, 5 as $t],
-        //             ];
-        //             for i in 0..2 {
-        //                 for j in 0..2 {
-        //                     assert!((((res[(i, j)] - target[(i, j)]) as f64).abs()) < std::f64::EPSILON);
-        //                 }
-        //             }
-        //         }
-        //     }
-        //
-        //     item! {
-        //         #[test]
-        //         #[should_panic]
-        //         fn [<test_scaledadd_mat_mat_panic_1_ $t>]() {
-        //             let a = array![
-        //                 [1 as $t],
-        //                 [3 as $t, 4 as $t],
-        //             ];
-        //             let b = array![
-        //                 [4 as $t, 3 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             let c = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             a.scaled_add(&b, &c);
-        //         }
-        //     }
-        //
-        //     item! {
-        //         #[test]
-        //         #[should_panic]
-        //         fn [<test_scaledadd_mat_mat_panic_2_ $t>]() {
-        //             let a = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [3 as $t, 4 as $t],
-        //             ];
-        //             let b = array![
-        //                 [4 as $t, 3 as $t],
-        //                 [1 as $t],
-        //             ];
-        //             let c = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             a.scaled_add(&b, &c);
-        //         }
-        //     }
-        //
-        //     item! {
-        //         #[test]
-        //         #[should_panic]
-        //         fn [<test_scaledadd_mat_mat_panic_3_ $t>]() {
-        //             let a = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [3 as $t, 4 as $t],
-        //             ];
-        //             let b = array![
-        //                 [4 as $t, 3 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             let c = array![
-        //                 [1 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             a.scaled_add(&b, &c);
-        //         }
-        //     }
-        //
-        //     item! {
-        //         #[test]
-        //         #[should_panic]
-        //         fn [<test_scaledadd_mat_mat_panic_4_ $t>]() {
-        //             let a = array![
-        //                 [1 as $t, 2 as $t],
-        //             ];
-        //             let b = array![
-        //                 [4 as $t, 3 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             let c = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             a.scaled_add(&b, &c);
-        //
-        //         }
-        //     }
-        //
-        //     item! {
-        //         #[test]
-        //         #[should_panic]
-        //         fn [<test_scaledadd_mat_mat_panic_5_ $t>]() {
-        //             let a = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [3 as $t, 4 as $t],
-        //             ];
-        //             let b = array![
-        //                 [4 as $t, 3 as $t],
-        //             ];
-        //             let c = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             a.scaled_add(&b, &c);
-        //         }
-        //     }
-        //
-        //     item! {
-        //         #[test]
-        //         #[should_panic]
-        //         fn [<test_scaledadd_mat_mat_panic_6_ $t>]() {
-        //             let a = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [3 as $t, 4 as $t],
-        //             ];
-        //             let b = array![
-        //                 [4 as $t, 3 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             let c = array![
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             a.scaled_add(&b, &c);
-        //         }
-        //     }
-        //
-        //     item! {
-        //         #[test]
-        //         fn [<test_scaledadd_mat_scalar_ $t>]() {
-        //             let a = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [3 as $t, 4 as $t],
-        //             ];
-        //             let b = 2 as $t;
-        //             let c = array![
-        //                 [1 as $t, 2 as $t],
-        //                 [2 as $t, 1 as $t],
-        //             ];
-        //             let res = a.scaled_add(&b, &c);
-        //             let target = array![
-        //                 [3 as $t, 6 as $t],
-        //                 [7 as $t, 6 as $t],
-        //             ];
-        //             for i in 0..2 {
-        //                 for j in 0..2 {
-        //                     assert!((((res[(i, j)] - target[(i, j)]) as f64).abs()) < std::f64::EPSILON);
-        //                 }
-        //             }
-        //         }
-        //     }
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_scaledadd_vec_vec_panic_1_ $t>]() {
+                    let a = array![1 as $t, 2 as $t];
+                    let b = array![3 as $t, 2 as $t, 1 as $t];
+                    let c = array![4 as $t, 5 as $t, 6 as $t];
+                    <Array1<$t> as ArgminScaledAdd<Array1<$t>, Array1<$t>, Array1<$t>>>::scaled_add(&a, &b, &c);
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_scaledadd_vec_vec_panic_2_ $t>]() {
+                    let a = array![1 as $t, 2 as $t, 3 as $t];
+                    let b = array![3 as $t, 2 as $t];
+                    let c = array![4 as $t, 5 as $t, 6 as $t];
+                    <Array1<$t> as ArgminScaledAdd<Array1<$t>, Array1<$t>, Array1<$t>>>::scaled_add(&a, &b, &c);
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_scaledadd_vec_vec_panic_3_ $t>]() {
+                    let a = array![1 as $t, 2 as $t, 3 as $t];
+                    let b = array![3 as $t, 2 as $t, 1 as $t];
+                    let c = array![4 as $t, 5 as $t];
+                    <Array1<$t> as ArgminScaledAdd<Array1<$t>, Array1<$t>, Array1<$t>>>::scaled_add(&a, &b, &c);
+                }
+            }
+
+            item! {
+                #[test]
+                fn [<test_scaledadd_mat_mat_ $t>]() {
+                    let a = array![
+                        [1 as $t, 2 as $t],
+                        [3 as $t, 4 as $t],
+                    ];
+                    let b = array![
+                        [4 as $t, 3 as $t],
+                        [2 as $t, 1 as $t],
+                    ];
+                    let c = array![
+                        [1 as $t, 2 as $t],
+                        [2 as $t, 1 as $t],
+                    ];
+                    let res = <Array2<$t> as ArgminScaledAdd<Array2<$t>, Array2<$t>, Array2<$t>>>::scaled_add(&a, &b, &c);
+                    let target = array![
+                        [5 as $t, 8 as $t],
+                        [7 as $t, 5 as $t],
+                    ];
+                    for i in 0..2 {
+                        for j in 0..2 {
+                            assert!((((res[(i, j)] - target[(i, j)]) as f64).abs()) < std::f64::EPSILON);
+                        }
+                    }
+                }
+            }
+
+            item! {
+                #[test]
+                fn [<test_scaledadd_mat_scalar_ $t>]() {
+                    let a = array![
+                        [1 as $t, 2 as $t],
+                        [3 as $t, 4 as $t],
+                    ];
+                    let b = 2 as $t;
+                    let c = array![
+                        [1 as $t, 2 as $t],
+                        [2 as $t, 1 as $t],
+                    ];
+                    let res = <Array2<$t> as ArgminScaledAdd<Array2<$t>, $t, Array2<$t>>>::scaled_add(&a, &b, &c);
+                    let target = array![
+                        [3 as $t, 6 as $t],
+                        [7 as $t, 6 as $t],
+                    ];
+                    for i in 0..2 {
+                        for j in 0..2 {
+                            assert!((((res[(i, j)] - target[(i, j)]) as f64).abs()) < std::f64::EPSILON);
+                        }
+                    }
+                }
+            }
         };
     }
 
