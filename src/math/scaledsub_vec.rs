635457c6e72d96b84b26376bfc1dd93f7f0502f7
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-02-02 07:20:39 +0100

    tests

--- src/math/scaledsub_vec.rs
+++ src/math/scaledsub_vec.rs
@@ -95,176 +95,176 @@ mod tests {
                 }
             }
 
-            // item! {
-            //     #[test]
-            //     fn [<test_scaledsub_mat_mat_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![3 as $t, 4 as $t],
-            //         ];
-            //         let b = vec![
-            //             vec![4 as $t, 3 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         let c = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         let res = a.scaled_sub(&b, &c);
-            //         let target = vec![
-            //             vec![5 as $t, 8 as $t],
-            //             vec![7 as $t, 5 as $t],
-            //         ];
-            //         for i in 0..2 {
-            //             for j in 0..2 {
-            //                 assert!((((res[i][j] - target[i][j]) as f64).abs()) < std::f64::EPSILON);
-            //             }
-            //         }
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     #[should_panic]
-            //     fn [<test_scaledsub_mat_mat_panic_1_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t],
-            //             vec![3 as $t, 4 as $t],
-            //         ];
-            //         let b = vec![
-            //             vec![4 as $t, 3 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         let c = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         a.scaled_sub(&b, &c);
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     #[should_panic]
-            //     fn [<test_scaledsub_mat_mat_panic_2_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![3 as $t, 4 as $t],
-            //         ];
-            //         let b = vec![
-            //             vec![4 as $t, 3 as $t],
-            //             vec![1 as $t],
-            //         ];
-            //         let c = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         a.scaled_sub(&b, &c);
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     #[should_panic]
-            //     fn [<test_scaledsub_mat_mat_panic_3_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![3 as $t, 4 as $t],
-            //         ];
-            //         let b = vec![
-            //             vec![4 as $t, 3 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         let c = vec![
-            //             vec![1 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         a.scaled_sub(&b, &c);
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     #[should_panic]
-            //     fn [<test_scaledsub_mat_mat_panic_4_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 2 as $t],
-            //         ];
-            //         let b = vec![
-            //             vec![4 as $t, 3 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         let c = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         a.scaled_sub(&b, &c);
-            //
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     #[should_panic]
-            //     fn [<test_scaledsub_mat_mat_panic_5_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![3 as $t, 4 as $t],
-            //         ];
-            //         let b = vec![
-            //             vec![4 as $t, 3 as $t],
-            //         ];
-            //         let c = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         a.scaled_sub(&b, &c);
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     #[should_panic]
-            //     fn [<test_scaledsub_mat_mat_panic_6_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![3 as $t, 4 as $t],
-            //         ];
-            //         let b = vec![
-            //             vec![4 as $t, 3 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         let c = vec![
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         a.scaled_sub(&b, &c);
-            //     }
-            // }
-            //
-            // item! {
-            //     #[test]
-            //     fn [<test_scaledsub_mat_scalar_ $t>]() {
-            //         let a = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![3 as $t, 4 as $t],
-            //         ];
-            //         let b = 2 as $t;
-            //         let c = vec![
-            //             vec![1 as $t, 2 as $t],
-            //             vec![2 as $t, 1 as $t],
-            //         ];
-            //         let res = a.scaled_sub(&b, &c);
-            //         let target = vec![
-            //             vec![3 as $t, 6 as $t],
-            //             vec![7 as $t, 6 as $t],
-            //         ];
-            //         for i in 0..2 {
-            //             for j in 0..2 {
-            //                 assert!((((res[i][j] - target[i][j]) as f64).abs()) < std::f64::EPSILON);
-            //             }
-            //         }
-            //     }
-            // }
+            item! {
+                #[test]
+                fn [<test_scaledsub_mat_mat_ $t>]() {
+                    let a = vec![
+                        vec![10 as $t, 20 as $t],
+                        vec![30 as $t, 40 as $t],
+                    ];
+                    let b = vec![
+                        vec![4 as $t, 3 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    let c = vec![
+                        vec![1 as $t, 2 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    let res = a.scaled_sub(&b, &c);
+                    let target = vec![
+                        vec![6 as $t, 14 as $t],
+                        vec![26 as $t, 39 as $t],
+                    ];
+                    for i in 0..2 {
+                        for j in 0..2 {
+                            assert!((((res[i][j] - target[i][j]) as f64).abs()) < std::f64::EPSILON);
+                        }
+                    }
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_scaledsub_mat_mat_panic_1_ $t>]() {
+                    let a = vec![
+                        vec![1 as $t],
+                        vec![3 as $t, 4 as $t],
+                    ];
+                    let b = vec![
+                        vec![4 as $t, 3 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    let c = vec![
+                        vec![1 as $t, 2 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    a.scaled_sub(&b, &c);
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_scaledsub_mat_mat_panic_2_ $t>]() {
+                    let a = vec![
+                        vec![1 as $t, 2 as $t],
+                        vec![3 as $t, 4 as $t],
+                    ];
+                    let b = vec![
+                        vec![4 as $t, 3 as $t],
+                        vec![1 as $t],
+                    ];
+                    let c = vec![
+                        vec![1 as $t, 2 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    a.scaled_sub(&b, &c);
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_scaledsub_mat_mat_panic_3_ $t>]() {
+                    let a = vec![
+                        vec![1 as $t, 2 as $t],
+                        vec![3 as $t, 4 as $t],
+                    ];
+                    let b = vec![
+                        vec![4 as $t, 3 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    let c = vec![
+                        vec![1 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    a.scaled_sub(&b, &c);
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_scaledsub_mat_mat_panic_4_ $t>]() {
+                    let a = vec![
+                        vec![1 as $t, 2 as $t],
+                    ];
+                    let b = vec![
+                        vec![4 as $t, 3 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    let c = vec![
+                        vec![1 as $t, 2 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    a.scaled_sub(&b, &c);
+
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_scaledsub_mat_mat_panic_5_ $t>]() {
+                    let a = vec![
+                        vec![1 as $t, 2 as $t],
+                        vec![3 as $t, 4 as $t],
+                    ];
+                    let b = vec![
+                        vec![4 as $t, 3 as $t],
+                    ];
+                    let c = vec![
+                        vec![1 as $t, 2 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    a.scaled_sub(&b, &c);
+                }
+            }
+
+            item! {
+                #[test]
+                #[should_panic]
+                fn [<test_scaledsub_mat_mat_panic_6_ $t>]() {
+                    let a = vec![
+                        vec![1 as $t, 2 as $t],
+                        vec![3 as $t, 4 as $t],
+                    ];
+                    let b = vec![
+                        vec![4 as $t, 3 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    let c = vec![
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    a.scaled_sub(&b, &c);
+                }
+            }
+
+            item! {
+                #[test]
+                fn [<test_scaledsub_mat_scalar_ $t>]() {
+                    let a = vec![
+                        vec![10 as $t, 20 as $t],
+                        vec![30 as $t, 40 as $t],
+                    ];
+                    let b = 2 as $t;
+                    let c = vec![
+                        vec![1 as $t, 2 as $t],
+                        vec![2 as $t, 1 as $t],
+                    ];
+                    let res = a.scaled_sub(&b, &c);
+                    let target = vec![
+                        vec![8 as $t, 16 as $t],
+                        vec![26 as $t, 38 as $t],
+                    ];
+                    for i in 0..2 {
+                        for j in 0..2 {
+                            assert!((((res[i][j] - target[i][j]) as f64).abs()) < std::f64::EPSILON);
+                        }
+                    }
+                }
+            }
         };
     }
 
