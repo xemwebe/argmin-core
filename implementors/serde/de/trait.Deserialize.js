a34f98d03338a19a980dac7f3c2b06a3ac09dd5a
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-02-06 17:02:39 +0100

    Semiautomatic update of documentation

---
+++ implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["argmin_core"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.87/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'static&gt; for <a class=\"struct\" href=\"argmin_core/struct.ArgminKV.html\" title=\"struct argmin_core::ArgminKV\">ArgminKV</a>",synthetic:false,types:["argmin_core::kv::ArgminKV"]},{text:"impl&lt;'de, T&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.87/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"argmin_core/struct.ArgminResult.html\" title=\"struct argmin_core::ArgminResult\">ArgminResult</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://docs.rs/serde/1.0.87/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt;,&nbsp;</span>",synthetic:false,types:["argmin_core::result::ArgminResult"]},{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.87/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"argmin_core/enum.TerminationReason.html\" title=\"enum argmin_core::TerminationReason\">TerminationReason</a>",synthetic:false,types:["argmin_core::termination::TerminationReason"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
