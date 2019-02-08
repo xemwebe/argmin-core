469ee024f64ee41be3d7dbb681d01240c0727501
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-02-08 16:12:02 +0100

    better serialization test

--- src/serialization.rs
+++ src/serialization.rs
@@ -5,11 +5,8 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use crate::nooperator::NoOperator;
 use crate::ArgminSolver;
 use serde::{Deserialize, Serialize};
-use std::default::Default;
-use std::fmt::Debug;
 
 pub trait ArgminSnapshot {
     fn store(&self) -> String;
@@ -18,10 +15,10 @@ pub trait ArgminSnapshot {
 
 impl<'de, T> ArgminSnapshot for T
 where
-    T: Serialize + Deserialize<'de>,
+    T: ArgminSolver + Serialize + Deserialize<'de>,
 {
     fn store(&self) -> String {
-        serde_json::to_string(self).unwrap()
+        serde_json::to_string_pretty(self).unwrap()
     }
 
     fn load() -> Self {
@@ -29,29 +26,53 @@ where
     }
 }
 
-// impl<'de, T, U, H> ArgminSnapshot for NoOperator<T, U, H>
-// where
-//     T: Clone + Default + Debug + Send + Sync + Serialize + Deserialize<'de>,
-//     U: Clone + Default + Debug + Send + Sync + Serialize + Deserialize<'de>,
-//     H: Clone + Default + Debug + Send + Sync + Serialize + Deserialize<'de>,
-// {
-//     fn store(&self) -> String {
-//         serde_json::to_string(self).unwrap()
-//     }
-//
-//     fn load() -> Self {
-//         unimplemented!();
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::nooperator::NoOperator;
+    use crate::*;
+    use argmin_codegen::ArgminSolver;
+
+    #[derive(ArgminSolver, Serialize, Deserialize, Clone)]
+    pub struct PhonySolver<T, O>
+    where
+        T: Clone + Default,
+        O: ArgminOperator<Parameters = T, OperatorOutput = f64, Hessian = ()>,
+    {
+        base: ArgminBase<T, f64, (), O>,
+    }
+
+    impl<T, O> PhonySolver<T, O>
+    where
+        T: Clone + Default + ArgminScaledSub<T, f64, T>,
+        O: ArgminOperator<Parameters = T, OperatorOutput = f64, Hessian = ()>,
+    {
+        /// Constructor
+        pub fn new(op: O, init_param: T) -> Self {
+            PhonySolver {
+                base: ArgminBase::new(op, init_param),
+            }
+        }
+    }
+
+    impl<T, O> ArgminNextIter for PhonySolver<T, O>
+    where
+        T: Clone + Default,
+        O: ArgminOperator<Parameters = T, OperatorOutput = f64, Hessian = ()>,
+    {
+        type Parameters = T;
+        type OperatorOutput = f64;
+        type Hessian = ();
+
+        fn next_iter(&mut self) -> Result<ArgminIterationData<Self::Parameters>, Error> {
+            unimplemented!()
+        }
+    }
 
     #[test]
     fn test_store() {
-        let op: NoOperator<Vec<f64>, f64, Vec<Vec<f64>>> = NoOperator::new();
-        println!("{:#?}", op.store());
+        let op: NoOperator<Vec<f64>, f64, ()> = NoOperator::new();
+        let solver = PhonySolver::new(op, vec![0.0, 0.0]);
+        println!("{}", solver.store());
     }
 }
