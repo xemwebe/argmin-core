22e9b407eb93e9d4284368a1a8a27d81007f0fa2
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-02-09 16:45:51 +0100

    Semiautomatic update of documentation

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["argmin_core"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"argmin_core/enum.CheckpointMode.html\" title=\"enum argmin_core::CheckpointMode\">CheckpointMode</a>",synthetic:false,types:["argmin_core::serialization::CheckpointMode"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
