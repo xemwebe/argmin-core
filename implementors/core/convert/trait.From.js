6f4a636dddd81b5445733881abb6585ee9b29fd5
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-03-27 10:13:04 +0100

    Semiautomatic update of documentation

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["argmin_core"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;&amp;'a <a class=\"struct\" href=\"argmin_core/struct.ArgminKV.html\" title=\"struct argmin_core::ArgminKV\">ArgminKV</a>&gt; for <a class=\"struct\" href=\"argmin_core/struct.ArgminSlogKV.html\" title=\"struct argmin_core::ArgminSlogKV\">ArgminSlogKV</a>",synthetic:false,types:["argmin_core::observers::slog_logger::ArgminSlogKV"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/slog/trait.KV.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["argmin_core"] = [{text:"impl KV for <a class=\"struct\" href=\"argmin_core/struct.ArgminSlogKV.html\" title=\"struct argmin_core::ArgminSlogKV\">ArgminSlogKV</a>",synthetic:false,types:["argmin_core::observers::slog_logger::ArgminSlogKV"]},{text:"impl&lt;O:&nbsp;<a class=\"trait\" href=\"argmin_core/trait.ArgminOp.html\" title=\"trait argmin_core::ArgminOp\">ArgminOp</a>&gt; KV for <a class=\"struct\" href=\"argmin_core/struct.IterState.html\" title=\"struct argmin_core::IterState\">IterState</a>&lt;O&gt;",synthetic:false,types:["argmin_core::iterstate::IterState"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
